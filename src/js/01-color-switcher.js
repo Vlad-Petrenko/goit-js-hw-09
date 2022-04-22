e5271fea4bc884dd2a3f1b61bd46c24a381b2b7a
Vlad-Petrenko <vlad120595@gmail.com>
2022-04-22 15:17:02 +0300

    colorChanger

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startBtnChangeColor: document.querySelector('[data-start]'),
+  stopBtnColor: document.querySelector('[data-stop]'),
+  allBody: document.querySelector('body'),
+};
+
+refs.startBtnChangeColor.addEventListener('click', changeColor);
+refs.stopBtnColor.addEventListener('click', stopChangeColor);
+let onChangeColor = 'null';
+
+function changeColor() {
+  onChangeColor = setInterval(() => {
+    refs.allBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtnChangeColor.disabled = true;
+}
+
+function stopChangeColor() {
+  clearInterval(onChangeColor);
+  refs.startBtnChangeColor.disabled = false;
+}
