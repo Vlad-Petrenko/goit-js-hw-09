0cbe8fcf2050281747ab2170ed680d9d0a6af4ac
Vlad-Petrenko <vlad120595@gmail.com>
2022-04-22 22:00:24 +0300

    timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,5 @@
+// import flatpickr from 'flatpickr';
+
+const flatpickr = require('flatpickr');
+// console.log('🚀 ~ file: 02-timer.js ~ line 4 ~ flatpickr', flatpickr);
+flatpickr('#datetime-picker', {});
