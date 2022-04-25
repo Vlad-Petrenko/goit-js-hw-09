79e67e8343c99bd2e2ddb04b959426a537f785c1
Vlad-Petrenko <92335560+Vlad-Petrenko@users.noreply.github.com>
2022-04-25 12:00:10 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,45 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  timerField: document.querySelector('.timer'),
+};
+refs.btnStart.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    console.log(options.defaultDate);
     if (selectedDates[0] <= options.defaultDate) {
       return window.alert('Please choose a date in the future');
+    } else {
+      refs.btnStart.disabled = false;
     }
+    const timer = setInterval(() => {
+      const deltaTime = selectedDates[0] - options.defaultDate;
+      const time = convertMs(deltaTime);
+      console.log(time);
+    }, 1000);
   },
 };
+
 flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
